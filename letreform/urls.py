4bbd083bef17063392b27282ba4bd554fe39506e
x0r5 <obivan05@gmail.com>
2019-10-07 14:47:57 +0200

    products app

--- letreform/urls.py
+++ letreform/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', products.urls),
+    path('', RedirectView.as_view(url='/products/', permanent=True)),
 ]
