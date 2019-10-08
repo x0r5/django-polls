a50705c410ae340067512bfe944897f7b85698f4
x0r5 <obivan05@gmail.com>
2019-10-08 14:41:23 +0200

    admin model reg

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from products.models import Category, Attribute, Group, Product, ProductPhoto
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(Group)
+admin.site.register(Product)
+admin.site.register(ProductPhoto)
